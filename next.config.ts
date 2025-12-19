c25c9eeea6d18f959876c5b595e422a0d4ea279c
[makarov4884] <[scg4884@naver.com]>
2025-12-19 11:35:34 +0900

    optimize build memory

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // 메모리 부족 방지 (무료 서버 배포용)
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
 
   output: "standalone",
